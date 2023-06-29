a553a24133a9b18d80d7fa27a05a35f77f7ff6dd
swapn9403 <130721572+swapn9403@users.noreply.github.com>
2023-06-29 18:52:19 +0900

    adding chatGPT translator

---
+++ translation_api.py
@@ -0,0 +1,34 @@
+import openai
+
+def translate_japanese_to_english(text):
+    # Replace 'YOUR_API_KEY' with your actual API key
+    api_key = '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'
+    
+    # Set up OpenAI API credentials
+    openai.api_key = api_key
+
+    # Compose the message
+    message = [
+        {'role': 'system', 'content': 'You are a helpful assistant that translates Japanese to English.'},
+        {'role': 'user', 'content': f'Translate "{text}" from Japanese to English.'}
+    ]
+
+    # Make the API request
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=message,
+        max_tokens=100
+    )
+    
+    # Process the response
+    for choice in response.choices:
+        if choice.role == 'assistant':
+            return choice.text.strip()
+    
+    return None
+
+
+# Example usage
+japanese_word = 'こんにちは'  # Replace with your Japanese word or phrase
+translation = translate_japanese_to_english(japanese_word)
+print(f'Translation: {translation}')
